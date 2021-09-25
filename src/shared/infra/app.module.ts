af8571750e81d1ba13e1789e067900aa2db9c399
Alex <alexs123655@gmail.com>
2021-09-25 17:42:44 -0300

    feat: typeorm config

--- src/shared/infra/app.module.ts
+++ src/shared/infra/app.module.ts
@@ -8,19 +8,25 @@ import { UserModule } from 'src/modules/users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env '}),
+    ConfigModule.forRoot({ envFilePath: '.env'}),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.TYPEORM_HOST,
-      port: 3306,
+      port: 5432,
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
       entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
-      synchronize: false,
+      migrations: ['dist/shared/infra/typeorm/migrations/*.{.ts,.js}'],
+      cli: {
+          migrationsDir: 'dist/shared/infra/typeorm/migrations'
+      },
+      synchronize: false
     }),
     UserModule,
     NewsModule
-  ]
+  ],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
