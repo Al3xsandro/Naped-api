203a10767f947592094e946a1b7272842ba894c0
Alex <alexs123655@gmail.com>
2021-09-28 07:54:09 -0300

    feat: class validator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './shared/infra/app.module';
 
+import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { 
       cors: true,
     });
+    app.useGlobalPipes(new ValidationPipe());
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import {
+  IsEmail,
+  IsNotEmpty
+} from 'class-validator';
+
 export class CreateUserDTO {
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   username: string;
+  
+  @IsNotEmpty()
   password: string;
 };
