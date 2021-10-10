7653a68323d710ccafc045f7294b90f12a6adc5b
Alex <alexs123655@gmail.com>
2021-10-10 17:28:22 -0300

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './shared/infra/http/app.module';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Naped API')
+    .setDescription('Naped - Reading the best news every day')
+    .setVersion('0.0.1')
+    .addTag('Naped')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document)
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
