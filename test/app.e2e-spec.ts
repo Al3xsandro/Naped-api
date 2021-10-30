c837131fd2818753f2430618d0efa97ea91b3bdf
Alex <alexs123655@gmail.com>
2021-10-30 14:25:47 -0300

    feat: Add e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,16 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/shared/infra/http/app.module';
+import { INestApplication } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-      controllers: [],
-      providers: []
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -22,6 +20,19 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(404)
   });
+
+
+  it('/news (GET)', async () => {
+    return request(app.getHttpServer())
+      .get('/news')
+      .expect(200)
+  })
+
+  it('/users/:username (GET)', async () => {
+    return request(app.getHttpServer())
+      .get('/users/john')
+      .expect(404)
+  })
 
   afterAll(async () => {
     await app.close();
