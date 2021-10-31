d51e95fbf204f6e85d08ee81d6d644c262b496d6
Alex <alexs123655@gmail.com>
2021-10-31 19:21:09 -0300

    fix: remove other tests to verify root route

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,40 +1,25 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/shared/infra/http/app.module';
 import { INestApplication } from '@nestjs/common';
-
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    jest.useFakeTimers()
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', async () => {
-    request(app.getHttpServer())
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
       .get('/')
       .expect(404)
   });
-
-
-  it('/news (GET)', async () => {
-    request(app.getHttpServer())
-      .get('/news')
-      .expect(200)
-  })
-
-  it('/users/:username (GET)', async () => {
-    request(app.getHttpServer())
-      .get('/users/john')
-      .expect(404)
-  })
 
   afterAll(async () => {
     await app.close();
